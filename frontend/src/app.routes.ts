ea117dff9866dac2695b2bf2ee053b07814dcca6
jphavill <jphavill@gmail.com>
2026-03-09 23:56:26 -0300

    create boilerplate angular frontend

--- frontend/src/app.routes.ts
+++ frontend/src/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 
 export const routes: Routes = [
   { path: '', component: AppComponent }
